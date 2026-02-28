f5a08aedf5fcef71dbc95a020fd9b1ff29fdc567
zeyxx <titouan.corneloup@protonmail.com>
2026-02-28 12:25:13 +0100

    feat: Add federation public API (__init__.py)

--- cynic/federation/__init__.py
+++ cynic/federation/__init__.py
@@ -1,3 +1,6 @@
-from cynic.federation.protocol import FederationMessage, FEDERATION_VERSION
+from cynic.federation.peer import FederationPeer
+from cynic.federation.protocol import FederationMessage
+from cynic.federation.gossip import GossipManager
+from cynic.federation.merge import merge_q_tables
 
-__all__ = ["FederationMessage", "FEDERATION_VERSION"]
+__all__ = ["FederationPeer", "FederationMessage", "GossipManager", "merge_q_tables"]
